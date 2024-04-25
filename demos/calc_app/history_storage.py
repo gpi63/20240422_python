fa79c1f54f6793671c1168f7c651ffb417216bc3
gpi63 <55111900+gpi63@users.noreply.github.com>
2024-04-25 11:44:47 -0700

    sync with instructor

--- demos/calc_app/history_storage.py
+++ demos/calc_app/history_storage.py
@@ -7,10 +7,10 @@ class HistoryStorage:
     def __init__(self, history: History) -> None:
         self.__history = history
 
-    def save_history(self, history_file) -> None:
-        with open(history_file, "w") as file:
+    def save_history(self, file_name: str) -> None:
+        with open(file_name, "w") as file:
             json.dump(list(self.__history), file)
 
-    def load_history(self, history_file) -> None:
-        with open(history_file, "r") as file:
+    def load_history(self, file_name: str) -> None:
+        with open(file_name, "r") as file:
             self.__history.bulk_import(json.load(file))
